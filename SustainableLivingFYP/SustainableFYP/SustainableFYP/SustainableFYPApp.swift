0e68395f3fabff64ef117a2055c1d578652d485c
KevinTerragon <157351665+KevinTerragon@users.noreply.github.com>
2025-09-10 09:05:21 +0800

    Add files via upload

---
+++ SustainableLivingFYP/SustainableFYP/SustainableFYP/SustainableFYPApp.swift
@@ -0,0 +1,33 @@
+//
+//  SustainableFYPApp.swift
+//  SustainableFYP
+//
+//  Created by SIT on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SustainableFYPApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ SustainableLivingFYP/SustainableFYP/SustainableFYPTests/SustainableFYPTests.swift
@@ -0,0 +1,17 @@
+//
+//  SustainableFYPTests.swift
+//  SustainableFYPTests
+//
+//  Created by SIT on 10/9/25.
+//
+
+import Testing
+@testable import SustainableFYP
+
+struct SustainableFYPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
